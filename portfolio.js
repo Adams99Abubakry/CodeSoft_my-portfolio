3484d149ea23a9bed8d88e86abfb1bb1e975212c
Adams99Abubakry <adamsabubakrsiddique@gmail.com>
2024-05-28 15:54:16 +0100

    corrections

--- portfolio.js
+++ portfolio.js
@@ -19,3 +19,16 @@ document.getElementById('contact-form').addEventListener('submit', function(even
     header.classList.toggle('sticky', window.scrollY > 0);
   });
   
+
+  function toggleText() {
+    var moreText = document.getElementById("moreText");
+    var btnText = document.getElementById("readMoreBtn");
+
+    if (moreText.style.display === "none") {
+        moreText.style.display = "inline";
+        btnText.innerHTML = "Read Less";
+    } else {
+        moreText.style.display = "none";
+        btnText.innerHTML = "Read More";
+    }
+}
